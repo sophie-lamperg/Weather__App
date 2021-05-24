f0fc761a2f81eab5e78c37d90942480edd88c645
Sophie <vasilevna-sofiya@mail.ru>
2021-05-24 23:49:33 +0300

    fix slider

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
                     <h1 className='header'>Weather <br/><span className="header__text">Forecast</span></h1>
                 </div>
                 <Main/>
-              <div className="page__background_desktop page__background_desktop-top"></div>
-              <div className="page__background_desktop page__background_desktop-bottom"></div>
+              
             </div>
-
+            <div className="page__background_desktop page__background_desktop-top"></div>
+              <div className="page__background_desktop page__background_desktop-bottom"></div>
             <footer><p className="footer__text">С любовью от MERCURY DEVELOPMENT</p></footer>
         </div>
     );
